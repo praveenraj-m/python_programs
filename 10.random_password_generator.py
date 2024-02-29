e9b5a9e0f743b8cfa06a9f1c7f11669c44477862
praveenraj-m <157500133+praveenraj-m@users.noreply.github.com>
2024-02-29 17:57:21 +0530

    10.random_password_generator.py

--- 10.random_password_generator.py
+++ 10.random_password_generator.py
@@ -1,12 +1,30 @@
-import random
-import string
+import random  # Import the random module for generating random values
+import string  # Import the string module for working with ASCII characters
 
-def generate_random_password(length):
+# Function to generate a random password of a given length
+def generate_random_password():
+    # Get the desired password length from the user
+    try:
+      password = input("Enter the desired password length: ")
+      password_length = int(password)
+    except ValueError:
+      print(f"Enter only numeric values to continue with password generation and not {password}")
+      return generate_random_password()
+
+    # Define the characters that can be used in the password
     characters = string.ascii_letters + string.digits + string.punctuation
-    password = ''.join(random.choice(characters) for _ in range(length))
+
+    # Initialize an empty string to store the generated password
+    password = ''
+
+    # Loop to randomly choose characters and concatenate them to form the password
+    for _ in range(password_length):
+        random_character = random.choice(characters)  # Randomly choose a character
+        password += random_character  # Concatenate the chosen character to the password
+
+    # Return the generated password
     return password
 
-# Example usage
-password_length = 12
-generated_password = generate_random_password(password_length)
+# Call the function to generate and print the password
+generated_password = generate_random_password()
 print("Generated Password:", generated_password)

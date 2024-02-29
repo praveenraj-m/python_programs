5cb0276f5c10d6bb01852e9ec707ef32596b758e
praveenraj-m <157500133+praveenraj-m@users.noreply.github.com>
2024-02-29 14:22:06 +0530

    07.basic_file_handling.py

--- 07.basic_file_handling.py
+++ 07.basic_file_handling.py
@@ -1,13 +1,15 @@
 # Basic File Handling
 def file_handling():
+    # Specify the file path
     file_path = 'example.txt'
 
     # Writing to a file
-    with open(file_path, 'w') as file:
-        file.write('Hello, this is a sample file.\n')
+    with open(file_path, 'a') as file:    # 'a" is used means it writes file and append without deleting the old data.
+        file.write('Hello, the text was written.\n')
 
     # Reading from a file
     with open(file_path, 'r') as file:
+        # Read the content of the file
         content = file.read()
         print(content)
 

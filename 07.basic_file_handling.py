ba581a38d38e7d72e2f0f8c202fa711dc486db29
praveenraj-m <157500133+praveenraj-m@users.noreply.github.com>
2024-03-01 12:14:21 +0530

    07.basic_file_handling.py

--- 07.basic_file_handling.py
+++ 07.basic_file_handling.py
@@ -3,15 +3,23 @@ def file_handling():
     # Specify the file path
     file_path = 'example.txt'
 
-    # Writing to a file
-    with open(file_path, 'a') as file:    # 'a" is used means it writes file and append without deleting the old data.
-        file.write('Hello, the text was written.\n')
+    # Writing to a file using 'w+' (Write and Read) mode (creating or truncating the file)
+    with open(file_path, 'w+') as file:
+        file.write('Hello, this is written using w+ mode.\n')
+        file.seek(0)  # Move the file pointer to the beginning
 
-    # Reading from a file
-    with open(file_path, 'r') as file:
-        # Read the content of the file
+        # Reading from a file using 'r+'(Read and Write) mode
         content = file.read()
-        print(content)
+        print(f'Content using w+ mode: {content}')
+
+    # Appending to a file using 'a' mode
+    with open(file_path, 'a') as file:
+        file.write('This is appended using a mode.\n')
+
+    # Reading from a file using 'r' mode
+    with open(file_path, 'r') as file:
+        content = file.read()
+        print(f'Final content: {content}')
 
 # Run the File Handling
 file_handling()
